e8295f84b8903d351e1493d726523137d65d33df
joelhagvall <joel.hagvall1@gmail.com>
2024-05-20 11:08:49 +0200

    New plot

--- visual/visualBarSubKeywords.py
+++ visual/visualBarSubKeywords.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
 # Data
-categories = ['Malware', 'Phishing', 'Hacking', 'Course']
-frequencies = [1896, 770, 612, 5282]
+categories = ['DDoS']
+frequencies = [159]
 
 # Create bar chart
 plt.figure(figsize=(10, 6))
